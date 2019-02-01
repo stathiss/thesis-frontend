59899761b935db2b6f054ed37a95a9e227c97376
stathiss <geor_stath@hotmail.com>
2019-02-01 15:55:06 +0200

    Connect with backend

--- src/App.js
+++ src/App.js
@@ -7,18 +7,11 @@ import NavBar from "./components/NavBar";
 import axios from "axios";
 
 class App extends Component {
-  constructor () {
-    super();
+  constructor (props) {
+    super(props);
     this.state = {
       username: ''
     };
-
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick () {
-    axios.get('https://api.github.com/users/maecapozzi')
-      .then(response => this.setState({username: response.data.name}))
   }
 
   render() {
@@ -34,10 +27,6 @@ class App extends Component {
             </Switch>
           </div>
         </BrowserRouter>
-        <div className='button__container'>
-          <button className='button' onClick={this.handleClick}>Click Me</button>
-          <p>{this.state.username}</p>
-        </div>
       </div>
     );
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,54 +1,91 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from '../assets/images/logo_2.png';
 import fear from '../assets/images/fear.png';
 import sadness from '../assets/images/sadness.png';
 import joy from '../assets/images/joy.png';
 import anger from '../assets/images/anger.png';
+import wheel from '../assets/images/wheel.png'
 
-import {Form,FormControl, FormGroup, ControlLabel, HelpBlock, Checkbox, Radio, Button} from "react-bootstrap";
+import {FormControl, FormGroup, ControlLabel, Button} from "react-bootstrap";
 
-const Home = () => {
+class Home extends Component {
 
-  return (
-    <div>
-      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '30vh'}}>
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: {},
+      isLoaded: false,
+      form: {hashtag: ''}
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-        <img src={logo} width={500} height={500} alt='logo'/>
-      </div>
+  handleClick() {
+    fetch('http://127.0.0.1:5000?hashtag=' + this.state.form.hashtag,
+    {
+      method: 'GET'
+    }).then(res => res.json())
+      .then(json => {
+        console.log(json);
+        console.log(this.state);
+        this.setState({
+          isLoaded: true,
+          items: json
+        });
+      });
+  }
+
+  handleChange(event) {
+    let fieldName = event.target.name;
+    let fleldVal = event.target.value;
+    this.setState({form: {...this.state.form, [fieldName]: fleldVal}})
+  }
+
+
+  render() {
+    const {_isLoaded, items} = this.state;
+    return (
+      <div>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '30vh'}}>
+
+          <img src={logo} width={500} height={500} alt='logo'/>
+        </div>
         <div className='plane'>
 
         </div>
-      <div className='intro-text' style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-        Tweet-ai has learned to understand emotions for english tweet. Type a hashtag to see what the program thinks
+        <div className='intro-text' style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          Tweet-ai has learned to understand emotions for english tweet. Type a hashtag to see what the program thinks
+        </div>
+        <div>
+          <p>&nbsp;</p>
+        </div>
+        <div>
+          <p>&nbsp;</p>
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <img src={fear} width={100} height={30} alt='fear'/>
+          <form onSubmit={e => { e.preventDefault(); this.handleClick();}}>
+            <FormGroup controlId="formControlsText">
+              <ControlLabel>Insert here: </ControlLabel>
+              <FormControl type="text" name="hashtag" placeholder="Enter hashtag text..." defaultValue={this.state.form.hashtag} onChange={this.handleChange} />
+            </FormGroup>
+            <Button className='button' onClick={this.handleClick}>
+              Submit
+            </Button>
+          </form>
+          <img src={sadness} width={100} height={40} alt='sadness'/>
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <img src={anger} width={120} height={35} alt='anger'/>
+          <img src={joy} width={120} height={50} alt='joy'/>
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          {items.hashtag}
+        </div>
       </div>
-      <div>
-        <p>&nbsp;</p>
-      </div>
-      <div>
-        <p>&nbsp;</p>
-      </div>
-      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-        <img src={fear} width={100} height={30} alt='fear'/>
-        <form>
-          <FormGroup controlId="formControlsText">
-            <ControlLabel>Insert here: </ControlLabel>
-            <FormControl type="text" placeholder="Enter hashtag text..." />
-          </FormGroup>
-
-          <Button type="submit">
-            Submit
-          </Button>
-          <div id="">
-          </div>
-        </form>
-        <img src={sadness} width={100} height={40} alt='sadness'/>
-      </div>
-      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-        <img src={anger} width={120} height={35} alt='anger'/>
-        <img src={joy} width={120} height={50} alt='joy'/>
-      </div>
-    </div>
-  );
-};
+    );
+  };
+}
 
 export default Home;
