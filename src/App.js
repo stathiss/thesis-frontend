04108f8c6d12f6b3cf713bb9e13e2c1ad778127c
George Stathis <gstathis@programize.com>
2019-02-17 22:42:39 +0200

    Tab and search field

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Error from "./components/Error";
 import NavBar from "./components/NavBar";
-import axios from "axios";
 
 class App extends Component {
   constructor (props) {
