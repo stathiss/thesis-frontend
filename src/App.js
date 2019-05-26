5614c83077b7efcb05006acd2a30f1f4a257c481
stathiss <geor_stath@hotmail.com>
2019-05-27 01:53:03 +0300

    Add e_c functionality

--- src/App.js
+++ src/App.js
@@ -26,10 +26,8 @@ class App extends Component {
             </Switch>
           </div>
         </BrowserRouter>
-          <div style={{width: "100%", textAlign: "center", position: "absolute", bottom: "5px" }}>
-            Proudly Developed by <strong><a href="http://www.linkedin.com/in/george-n-stathis" target="_blank">George Stathis</a></strong>
-          </div>
       </div>
+
     );
   }
 }
